bbfe7ee24c338f106a37b9dfdda5a18ff6b24e79
romangorbacev <rn.gorbachev@gmail.com>
2023-11-06 17:29:57 +0600

    [4] - Created contact module and added contact.component.ts

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 
 
@@ -9,7 +10,9 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink,
+    RouterLinkActive
   ],
   exports: [
     HeaderComponent
